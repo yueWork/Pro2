c615ab951e7f721b885eb35c8794454d663a7d42
yue-b <yue@byuedembp.lan>
2015-11-08 16:27:23 +0800

    Initial Commit

---
+++ Pro2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Pro2
+//
+//  Created by byue on 15/11/8.
+//  Copyright © 2015年 byue. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
